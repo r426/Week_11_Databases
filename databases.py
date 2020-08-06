f1859daa547777918fcdd5262ed3604123894747
Your Name <rutaslv@hotmail.lt>
2020-08-06 20:01:36 +0300

    print the output

--- databases.py
+++ databases.py
@@ -10,18 +10,25 @@ cursor = conn.cursor()
 try:
     # cursor.execute("CREATE TABLE tourneys(id int, name varchar(30), wins int, best int, size int);")
     cursor.execute("INSERT INTO tourneys values(1, 'John', 10, 240, 40);")
-    cursor.execute("INSERT INTO tourneys values(2, 'Adda', 12, 240, 39);")
-    cursor.execute("INSERT INTO tourneys values(3, 'Lynn', 8, 240, 38);")
+    cursor.execute("INSERT INTO tourneys values(2, 'Adda', 12, 140, 39);")
+    cursor.execute("INSERT INTO tourneys values(3, 'Lynn', 8, 200, 38);")
     cursor.execute("INSERT INTO tourneys values(4, 'Roy', 14, 240, 42);")
-    cursor.execute("INSERT INTO tourneys values(5, 'Tom', 9, 240, 41);")
-    cursor.execute("INSERT INTO tourneys values(6, 'Jake', 10, 240, 40);")
-    cursor.execute("INSERT INTO tourneys values(7, 'Rose', 11, 240, 37);")
-    cursor.execute("INSERT INTO tourneys values(8, 'Ted', 7, 240, 40);")
-    cursor.execute("INSERT INTO tourneys values(9, 'Jeff', 9, 240, 40);")
-    cursor.execute("INSERT INTO tourneys values(10, 'Sue', 15, 240, 38);")
+    cursor.execute("INSERT INTO tourneys values(5, 'Tom', 9, 100, 41);")
+    cursor.execute("INSERT INTO tourneys values(6, 'Jake', 10, 160, 40);")
+    cursor.execute("INSERT INTO tourneys values(7, 'Rose', 11, 210, 37);")
+    cursor.execute("INSERT INTO tourneys values(8, 'Ted', 7, 180, 40);")
+    cursor.execute("INSERT INTO tourneys values(9, 'Jeff', 9, 120, 40);")
+    cursor.execute("INSERT INTO tourneys values(10, 'Sue', 15, 270, 38);")
     conn.commit()
 except:
     conn.rollback()
+
+cursor.execute('SELECT * FROM tourneys')
+rows = cursor.fetchall()
+print("Total Number of records", cursor.rowcount)
+
+for row in rows:
+    print(row)
 
 cursor.close()
 conn.close()

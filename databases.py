ddf66c369a6264915c227a4e327172d343747b0f
Your Name <rutaslv@hotmail.lt>
2020-08-06 19:52:17 +0300

    populate the table

--- databases.py
+++ databases.py
@@ -8,9 +8,18 @@ if conn.is_connected():
 cursor = conn.cursor()
 
 try:
-    cursor.execute("CREATE TABLE tourneys(id int, name varchar(30), wins int, best int, size int);")
+    # cursor.execute("CREATE TABLE tourneys(id int, name varchar(30), wins int, best int, size int);")
+    cursor.execute("INSERT INTO tourneys values(1, 'John', 10, 240, 40);")
+    cursor.execute("INSERT INTO tourneys values(2, 'Adda', 12, 240, 39);")
+    cursor.execute("INSERT INTO tourneys values(3, 'Lynn', 8, 240, 38);")
+    cursor.execute("INSERT INTO tourneys values(4, 'Roy', 14, 240, 42);")
+    cursor.execute("INSERT INTO tourneys values(5, 'Tom', 9, 240, 41);")
+    cursor.execute("INSERT INTO tourneys values(6, 'Jake', 10, 240, 40);")
+    cursor.execute("INSERT INTO tourneys values(7, 'Rose', 11, 240, 37);")
+    cursor.execute("INSERT INTO tourneys values(8, 'Ted', 7, 240, 40);")
+    cursor.execute("INSERT INTO tourneys values(9, 'Jeff', 9, 240, 40);")
+    cursor.execute("INSERT INTO tourneys values(10, 'Sue', 15, 240, 38);")
     conn.commit()
-    print('Table "tourneys" created.')
 except:
     conn.rollback()
 

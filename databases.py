dace065d1e3dcbbfaac50a5624276419099fcde7
Your Name <rutaslv@hotmail.lt>
2020-08-06 19:38:19 +0300

    create the "tourneys" table

--- databases.py
+++ databases.py
@@ -5,4 +5,16 @@ conn = mysql.connector.connect(host='localhost', database='friendsdb', user='roo
 if conn.is_connected():
     print("Connected to mysql friendsdb")
 
+cursor = conn.cursor()
+
+try:
+    cursor.execute("CREATE TABLE tourneys(id int, name varchar(30), wins int, best int, size int);")
+    conn.commit()
+    print('Table "tourneys" created.')
+except:
+    conn.rollback()
+
+cursor.close()
+conn.close()
+
 conn.close()
